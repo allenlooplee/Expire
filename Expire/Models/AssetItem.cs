e2e2f912affdde0d41af4ec20166ecc6255fdedf
Allen Lee <looplee@msn.com>
2017-06-22 10:25:00 +0800

    Add AssetItem model.

---
+++ Expire/Models/AssetItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Expire.Models
+{
+    public class AssetItem
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Remark
+        {
+            get;
+            set;
+        }
+
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+    }
+}
